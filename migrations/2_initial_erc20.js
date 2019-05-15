6593ead4563ac549b8e3c963c5b8e93c431c2ec2
ooozws <weisaizhang@gmail.com>
2019-05-15 10:19:46 +0800

    update

---
+++ migrations/2_initial_erc20.js
@@ -0,0 +1,19 @@
+var ERC20 = artifacts.require("../contracts/Erc20.c")
+
+module.exports = function (deployer) {
+    deployer.deploy(ERC20, "1000000", "bitcoin", "BTC").then(function (instance) {
+        deploy = instance;
+        return deploy.GetTotalSupply()
+    }).then(function (totalSupply) {
+        console.log("totalSupply", totalSupply.toString());
+        return deploy.GetDecimals();
+    }).then(function (decimals) {
+        console.log("decimals", decimals.toString());
+        return deploy.GetTokenName();
+    }).then(function (tokenName) {
+        console.log("tokenName", tokenName);
+        return deploy.GetAmount("0x122369f04f32269598789998de33e3d56e2c507a")
+    }).then(function (balance) {
+        console.log("balance", balance.toString());
+    })
+};
