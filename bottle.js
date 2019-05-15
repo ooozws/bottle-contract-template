b56d8560cacea965647c83c750ad50d66d653c0e
ooozws <weisaizhang@gmail.com>
2019-05-15 14:34:02 +0800

    update

--- bottle.js
+++ bottle.js
@@ -3,18 +3,10 @@ module.exports = {
     development: {
       host: "127.0.0.1",
       port: 8880,
-      network_id: '*',
+      network_id: '*', // Match any network id
       gas: 4700000,
       gasPrice: 20000000000,
-      from: "0x122369f04f32269598789998de33e3d56e2c507a"
-    },
-    live: {
-      host: "127.0.0.1",
-      port: 8880,
-      network_id: '*',
-      gas: 4700000,
-      gasPrice: 20000000000,
-      from: "0x122369f04f32269598789998de33e3d56e2c507a"
+      // from: "0x122369f04f32269598789998de33e3d56e2c507a"
     },
   },
 };
